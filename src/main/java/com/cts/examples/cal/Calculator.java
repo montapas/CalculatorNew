030af68565aa8b4527c0cba9aa91b3dc5e636138
montapas <montapas@gmail.com>
2018-11-14 12:56:05 +0530

    Add files via upload

---
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -0,0 +1,32 @@
+package com.cts.examples.cal;
+
+public class Calculator implements ICalculator {
+
+	public int sum(int a, int b) {
+		 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
+		return a + b;  
+	}
+	 
+	public int subtraction(int a, int b) {
+	       return a - b;
+	}
+	 
+	public int multiplication(int a, int b) {
+		 
+		return a * b;
+	}
+
+	public int divison(int a, int b)  {
+		
+		return a / b;
+	}
+	 
+	public boolean equalIntegers(int a, int b) {
+		boolean result = false;
+            	if (a == b) {
+			result = true;
+		}
+ 
+		return result;
+	}
+}
