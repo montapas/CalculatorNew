70a446d965a6496ba0e50088cf71cc3ec7f09522
montapas <montapas@gmail.com>
2018-11-20 14:09:49 +0530

    Update Calculator.java

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -7,9 +7,7 @@ public class Calculator implements ICalculator {
 	}
 	 
 	public int subtraction(int a, int b) {
-		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
-		
-	       return a - b;
+		return a - b;
 	}
 	 
 	public int multiplication(int a, int b) {
